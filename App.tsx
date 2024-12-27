b25f48de5f6502915e2cd7df9c6e946e28f7be58
TanakaSenpai <sakifshahedur7@gmail.com>
2024-12-27 16:36:21 +0600

    Moved App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import {SafeAreaView} from "react-native-safe-area-context"
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import TabNavigator from "./navigation/TabNavigator";
+import TabNavigator from "./app/navigation/TabNavigator";
 
 export default function App() {
   return (
@@ -11,12 +11,11 @@ export default function App() {
         <TabNavigator />
       </NavigationContainer>
     </SafeAreaView>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
-})
+    flex: 1,
+  },
+});
